b68cf50b9a5398665fba9c050c5ab36e2a0331af
Piyush Kukadiya <piyushkukadiya1@gmail.com>
2022-03-26 19:54:54 +0000

    Date and Notes

--- Spending.py
+++ Spending.py
@@ -1,4 +1,5 @@
 from Category import Category
+from Transaction import Transaction
 from datetime import datetime
 class Spending:
 
@@ -15,21 +16,29 @@ class Spending:
         #s1 = Spending()
         #self.date()
         c1 = Category()
+        t1 = Transaction()
         c1.cat_print()
         choose_cat = int(input("Choose category Between 0 To 4: "))
         a1 = c1.list_category[choose_cat]
+
+        your_date = input("Enter date in given format yyyy-mm-dd: ")
+        # Create date object in given time format yyyy-mm-dd
+        my_date = datetime.strptime(your_date, "%Y-%m-%d")
+
         amount = int(input("Enter Amount: "))
         a2 = c1.list_budget[choose_cat] - amount
         c1.add_budget(choose_cat,a2)
-        print(a1, a2)
+        #print(a1, a2, my_date)
         note = input("Enter Note: ")
-        print(amount, choose_cat, note)
+        #t1.add_notes(choose_cat,note)
+        print(amount, choose_cat, note, my_date)
 
     def income(self):
         c1 = Category()
         c1.cat_print()
         choose_cat = int(input("Choose category Between 0 To 4: "))
         a1 = c1.list_category[choose_cat]
+        self.current_date()
         amount = int(input("Enter Amount: "))
         a2 = c1.list_budget[choose_cat] + amount
         c1.add_budget(choose_cat, a2)
--- Transaction.py
+++ Transaction.py
@@ -1,3 +1,12 @@
+from Category import Category
+class Transaction:
+    list_notes = []
 
+    def add_notes(self, catID, notes):
+        self.list_notes[catID] = notes
+
+    def edit_transaction(self):
+        c1 = Category()
+        c1.cat_print()
 
 
